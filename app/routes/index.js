1ba534d5a57b6d5a33a367233021f00e8469d35b
Patrick McGreevy <patrick7490@icloud.com>
2017-03-27 05:53:07 -0700

    Display questions on homepage

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
+var questions = [{
+  title: 'Who are you?',
+  author: 'John Doe',
+  notes: 'lorem ipsum'
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+    // return this.store.findAll('question');
+  }
 });
